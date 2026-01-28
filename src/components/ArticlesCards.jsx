5dc9188c94bc65c0db363706cde51bdcfc3f1519
unknown <benoit.mosele@gmail.com>
2026-01-28 22:17:24 +0100

    feature/pictureOnCard

--- src/components/ArticlesCards.jsx
+++ src/components/ArticlesCards.jsx
@@ -1,21 +1,38 @@
+import React from 'react';
+
 const ArticlesCards = ({ article }) => {
-  return <>
+    const BASE_URL = "http://localhost:3000";
+    const imageUrl = article.picture ? `${BASE_URL}/${article.picture}` : null;
 
-    {/* Partie Gauche */}
-    <div className="card">
-      <div className="cardPicture">
-        <img src={article.picture} alt={article.title} />
-      </div>
+    return (
+        <div className="card">
+            {/* BLOC GAUCHE : IMAGE */}
+            <div className="cardPicture">
+                {imageUrl ? (
+                    <img src={imageUrl} alt={article.title} />
+                ) : (
+                    // Image par défaut si pas de photo
+                    <div style={{width:"100%", height:"100%", background:"#eee", display:"flex", alignItems:"center", justifyContent:"center"}}>
+                        <span>No Image</span>
+                    </div>
+                )}
+            </div>
 
-      {/* Partie droite */}
-      <div className="cardBody">
-        <h3>{article.title}</h3>
-        <p>Publié le : {new Date(article.publicationDate).toLocaleDateString()} par <strong>
-          {article.firstName} {article.lastName}</strong></p>
-        <button>Lire l'article</button>
-      </div>
-    </div>
-  </>;
-}
+            {/* BLOC DROITE : TEXTE */}
+            <div className="cardBody">
+                <h2>{article.title}</h2>
+                
+                <p>
+                    Publié le {new Date(article.publicationDate).toLocaleDateString()} 
+                    {/* Ici on utilise les données du JOIN SQL */}
+                    <br/>
+                    par <strong>{article.firstName} {article.lastName}</strong>
+                </p>
+                
+                <button>Lire l'article</button>
+            </div>
+        </div>
+    );
+};
 
 export default ArticlesCards;
